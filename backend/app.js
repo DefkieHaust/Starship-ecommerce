d41fe532b5fc764caa90233d09ac321fe543f56e
DefkieHaust <noreply>
2023-08-25 17:53:05 +0500

    fixed cors issue

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,18 @@ const morgan = require('morgan')
 const dotenv = require('dotenv')
 
 const errorMiddleware = require('./middleware/error');
+const ErrorHandler = require('./utils/errorHandler');
 
 dotenv.config({path: './config/.env'})
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS, HEAD");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+})
 
 // loggers
 app.use(morgan("combined"))
